d90f1cd1e43bd2c0b876bdd51da657513906a3a6
Shakil <shakilahmmed8882@gmail.com>
2024-02-02 02:50:40 -0800

    Disabled socket connection for the absance of providers

--- src/middlewares/applyMiddleware.js
+++ src/middlewares/applyMiddleware.js
@@ -1,24 +1,26 @@
-const cors = require('cors');
+const cors = require("cors");
 const express = require("express");
 const cookieParser = require("cookie-parser");
 
 const applyMiddleWare = (app) => {
-  // Middleware 
-  app.use(cors({
-    origin: "http://localhost:3000",
-    methods: ["GET", "PATCH"],
-    credentials: true,
-    optionsSuccessStatus: 204,
-  }));
+
+  // Middleware
+  app.use(
+    cors({
+      origin: "*",
+      methods: ["GET", "PATCH", "POST", "DELETE"],
+      credentials: true,
+      optionsSuccessStatus: 204,
+    })
+  );
 
   app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Methods', 'GET, PATCH'); 
+    res.header("Access-Control-Allow-Methods", "GET, PATCH, PUT, DELETE, POST");
     next();
   });
 
   app.use(express.json());
   app.use(cookieParser());
 };
-
 
 module.exports = applyMiddleWare;
