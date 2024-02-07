4f8f06641ee6ecb43704303e2336f7ef28fe0e79
Shakil <shakilahmmed8882@gmail.com>
2024-02-07 02:27:51 -0800

    chore(refractor):renamed collection names and format the code

--- src/api/routes/index.js
+++ src/api/routes/index.js
@@ -18,28 +18,28 @@ const router = express.Router();
 const initializeRoutes = async () => {
   try {
     // Create database collections
-    const tasksCollection = await createDB("tasks");
-    const usersCollection = await createDB("users");
-    const workspace = await createDB("workspace");
+    const tasks = await createDB("tasks");
+    const users = await createDB("users");
+    const worspace = await createDB("workspace");
 
     // Task related APIs
-    router.get("/tasks", async (req, res) => await getAllTasks(req, res, tasksCollection));
-    router.post("/createTask", async (req, res) => await CreateTask(req, res, tasksCollection));
-    router.put("/updateTask/:id", async (req, res) => await updateTask(req, res, tasksCollection));
-    router.patch("/updateTaskState", async (req, res) => await updateTaskState(req, res, tasksCollection));
-    router.delete("/deleteTask/:id", async (req, res) => await deleteTask(req, res, tasksCollection));
+    router.get("/tasks", async (req, res) => await getAllTasks(req, res, tasks));
+    router.post("/createTask", async (req, res) => await CreateTask(req, res, tasks));
+    router.put("/updateTask/:id", async (req, res) => await updateTask(req, res, tasks));
+    router.patch("/updateTaskState", async (req, res) => await updateTaskState(req, res, tasks));
+    router.delete("/deleteTask/:id", async (req, res) => await deleteTask(req, res, tasks));
 
     // User related APIs
-    router.get("/users", async (req, res) => await getAllUsers(req, res, usersCollection));
-    router.get("/users/:email", async (req, res) => await getSingleUser(req, res, usersCollection));
-    router.post("/users", async (req, res) => await CreateUser(req, res, usersCollection));
-    router.put("/users/:email", async (req, res) => await updateUser(req, res, usersCollection));
-    router.put("/userImage/:email", async (req, res) => await updateUserImage(req, res, usersCollection));
+    router.get("/users", async (req, res) => await getAllUsers(req, res, users));
+    router.get("/users/:email", async (req, res) => await getSingleUser(req, res, users));
+    router.post("/users", async (req, res) => await CreateUser(req, res, users));
+    router.put("/users/:email", async (req, res) => await updateUser(req, res, users));
+    router.put("/userImage/:email", async (req, res) => await updateUserImage(req, res, users));
 
     // Workspace related APIs
-    router.get("/workspaces", async (req, res) => await workspaces(req, res, workspace));
-    router.get("/workspace-tasks/:workspace/:creator", async (req, res) => await getWorkspaceTask(req, res, tasksCollection));
-    router.post("/create-workspace", async (req, res) => await createWorkspace(req, res, workspace));
+    router.get("/workspaces", async (req, res) => await workspaces(req, res, worspace));
+    router.get("/workspace-tasks/:workspace/:creator", async (req, res) => await getWorkspaceTask(req, res, tasks));
+    router.post("/create-workspace", async (req, res) => await createWorkspace(req, res, worspace));
 
     // Payment related API
     router.post("/create-payment-intent", async (req, res) => await PaymentIntend(req, res));
