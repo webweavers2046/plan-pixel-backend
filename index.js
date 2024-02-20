daf55e4bbebbf4e27140adae9aeed38299033769
Forhad96 <forhadairdrop@gmail.com>
2024-02-20 14:01:54 +0600

    socket io connection and transfer real time data message implemnt

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-
 const connectDB = require("./src/db/connectDB");
 const express = require("express");
 const applyMiddleWare = require("./src/middlewares/applyMiddleware");
@@ -7,11 +6,26 @@ require("dotenv").config();
 const port = process.env.PORT || 5000;
 const app = express();
 
+// socket io integration
+const { messageSocketFunc } = require("./src/api/controllers/message");
+const http = require("http").Server(app);
+const cors = require("cors");
+const socketIO = require("socket.io")(http, {
+  cors: {
+    origin: "http://localhost:3000",
+  },
+});
+
+app.use(cors());
+messageSocketFunc(socketIO);
+
 // Add Ably integration
 const Ably = require("ably");
 const { ObjectId } = require("mongodb");
 const ably = new Ably.Realtime(process.env.ABLY_KEY);
 const channel = ably.channels.get("tasks"); // Choose a channel name
+
+
 
 // Middleware setup
 applyMiddleWare(app);
@@ -24,8 +38,7 @@ app.get("/", (req, res) => {
 let tasksCollection;
 let workspaceCollection;
 let usersCollection;
-let lastModifiedDocumentId = ""; 
-
+let lastModifiedDocumentId = "";
 
 // Connect to MongoDB
 const client = createMongoClient();
@@ -40,59 +53,67 @@ connectDB(app, () => {
       tasksCollection = database.collection("tasks");
       workspaceCollection = database.collection("workspace");
       usersCollection = database.collection("users");
-        
- 
-  // ============================== Task collection tracking =======================
-  const changeStreamTasks = tasksCollection.watch();
 
-  // publish latest task when task collection contents get modified
-  changeStreamTasks.on("change", async (changeEvent) => {
-    try {
-      
-    // Check if documentId is updated
-    // const updatedDocumentId = changeEvent.documentKey._id.toString();
-    const updatedDocumentId = changeEvent.documentKey._id.toString();
+      // ============================== Task collection tracking =======================
+      const changeStreamTasks = tasksCollection.watch();
 
-    const lastChangedId = await tasksCollection.findOne(
-      { _id: new ObjectId(updatedDocumentId) },
-      { projection: { _id:0,lastModifiedBy: 1 } }
-    );
-    
-    const userEmail = lastChangedId?.lastModifiedBy
-    const user = await usersCollection.findOne({ email: userEmail });
-    const activeWorkspace = await workspaceCollection.findOne({_id: new ObjectId(user?.activeWorkspace)}) 
-    const userWokspaceIds = await user?.workspaces?.map(id => new ObjectId(id))
-    const userWorkspaces = await workspaceCollection?.find({ _id: { $in: userWokspaceIds } }).toArray();
-    
-    const workspaceTasksIds = activeWorkspace?.tasks?.map(workspaceId => new ObjectId(workspaceId))
-    const allTasksInWorkspace = await tasksCollection?.find({ _id: { $in: workspaceTasksIds } }).toArray();
-    
-    const workspaceMembersEmails = activeWorkspace?.members?.map(member => member)
-    const allMembersInWorkspace = await usersCollection?.find({ email: { $in: workspaceMembersEmails } }).toArray();
+      // publish latest task when task collection contents get modified
+      changeStreamTasks.on("change", async (changeEvent) => {
+        try {
+          // Check if documentId is updated
+          // const updatedDocumentId = changeEvent.documentKey._id.toString();
+          const updatedDocumentId = changeEvent.documentKey._id.toString();
 
+          const lastChangedId = await tasksCollection.findOne(
+            { _id: new ObjectId(updatedDocumentId) },
+            { projection: { _id: 0, lastModifiedBy: 1 } }
+          );
 
-    channel.publish('workspaces', {
-      allWorkspaces:userWorkspaces,
-      allMembersInWorkspace,
-      allTasksInWorkspace,
-      activeWorkspace
-    });
-    
-    
-    } catch (error) {
-      console.error("Error reloading and emitting tasks:", error);
-    }
-  });
+          const userEmail = lastChangedId?.lastModifiedBy;
+          const user = await usersCollection.findOne({ email: userEmail });
+          const activeWorkspace = await workspaceCollection.findOne({
+            _id: new ObjectId(user?.activeWorkspace),
+          });
+          const userWokspaceIds = await user?.workspaces?.map(
+            (id) => new ObjectId(id)
+          );
+          const userWorkspaces = await workspaceCollection
+            ?.find({ _id: { $in: userWokspaceIds } })
+            .toArray();
 
+          const workspaceTasksIds = activeWorkspace?.tasks?.map(
+            (workspaceId) => new ObjectId(workspaceId)
+          );
+          const allTasksInWorkspace = await tasksCollection
+            ?.find({ _id: { $in: workspaceTasksIds } })
+            .toArray();
 
+          const workspaceMembersEmails = activeWorkspace?.members?.map(
+            (member) => member
+          );
+          const allMembersInWorkspace = await usersCollection
+            ?.find({ email: { $in: workspaceMembersEmails } })
+            .toArray();
+
+          channel.publish("workspaces", {
+            allWorkspaces: userWorkspaces,
+            allMembersInWorkspace,
+            allTasksInWorkspace,
+            activeWorkspace,
+          });
+        } catch (error) {
+          console.error("Error reloading and emitting tasks:", error);
+        }
+      });
     })
     .catch((error) => {
       console.error("Error loading initial tasks:", error);
     });
 });
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+// app.listen(port, () => {
+//   console.log(`Server is running on port ${port}`);
+// });
+http.listen(port, () => {
+  console.log(`Server listening on ${port}`);
 });
-
-
 module.exports = app;
---
+++ src/api/controllers/message/index.js
@@ -0,0 +1,32 @@
+const messageSocketFunc = (socketIO)=>{
+let users = [];
+
+socketIO.on("connection", (socket) => {
+  console.log(`⚡: ${socket.id} user just connected!`);
+  socket.on("message", (data) => {
+    socketIO.emit("messageResponse", data);
+  });
+  socket.on("typing", (data) => socket.broadcast.emit("typingResponse", data));
+
+  //Listens when a new user joins the server
+  socket.on("newUser", (data) => {
+    //Adds the new user to the list of users
+    users.push(data);
+    // console.log(users);
+    //Sends the list of users to the client
+    socketIO.emit("newUserResponse", users);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("🔥: A user disconnected");
+    //Updates the list of users when a user disconnects from the server
+    users = users.filter((user) => user.socketID !== socket.id);
+    // console.log(users);
+    //Sends the list of users to the client
+    socketIO.emit("newUserResponse", users);
+    socket.disconnect();
+  });
+});
+}
+
+module.exports = {messageSocketFunc}
