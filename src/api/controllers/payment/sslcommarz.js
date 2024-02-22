3490559300f2a6dcf5d1576e3fb105c988660821
Web Weavers <156602277+webweavers2046@users.noreply.github.com>
2024-02-22 20:04:45 +0600

    Update sslcommarz.js

--- src/api/controllers/payment/sslcommarz.js
+++ src/api/controllers/payment/sslcommarz.js
@@ -127,16 +127,16 @@ const paymentFailed = async (req, res, tasksCollection) => {
       { transactionId: req.params.transId }
     );
     if(result.deletedCount){
-            // const baseUrl =
-            //   app.settings.env === "development"
-            //     ? "http://localhost:3000"
-            //     : "https://plan-pixel.vercel.app/"; // Replace 'https://your-production-url.com' with your actual production URL
+            const baseUrl =
+              app.settings.env === "development"
+                ? "http://localhost:3000"
+                : "https://plan-pixel.vercel.app/"; // Replace 'https://your-production-url.com' with your actual production URL
 
 
-            // res.redirect(`${baseUrl}/payment-failed`);
-      res.redirect(
-        `https://plan-pixel.vercel.app/payment-failed`
-      );
+            res.redirect(`${baseUrl}/payment-failed`);
+      // res.redirect(
+      //   `https://plan-pixel.vercel.app/payment-failed`
+      // );
     }
   } catch (error) {
     console.log(error);
