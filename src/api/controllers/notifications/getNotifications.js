aebcac462895e1f2b8d1c1a43281b21bac0c0264
SabbirSami <smd71430@gmail.com>
2024-03-01 15:36:57 +0600

    add number of premium members api

--- src/api/controllers/notifications/getNotifications.js
+++ src/api/controllers/notifications/getNotifications.js
@@ -1,16 +1,16 @@
 const { ObjectId } = require("mongodb");
 
 const getNotifications = async (req, res, workspaces) => {
-
-    const workspaceId = req.params.activeWorkspaceId
+    const workspaceId = req.params.activeWorkspaceId;
 
     try {
-      const workspace = await workspaces.findOne({ _id: new ObjectId(workspaceId) });
-      res.send(workspace?.notifications);
+        const workspace = await workspaces.findOne({
+            _id: new ObjectId(workspaceId),
+        });
+        res.send(workspace?.notifications);
     } catch (error) {
-      res.status(500).send("Internal Server Error");
+        res.status(500).send("Internal Server Error");
     }
-  };
+};
 
-
- module.exports = {getNotifications}
+module.exports = { getNotifications };
